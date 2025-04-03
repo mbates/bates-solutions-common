b0f070126b52958d97e00814c701f06233d8725e
Mike Bates <mike@bates-solutions.com>
2025-04-02 19:48:38 -0700

    feat: add error components

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 // export everything from errors and middleswares
 
-// export * from "./errors/bad-request-error";
-// export * from "./errors/custom-error";
-// export * from "./errors/forbidden-error";
-// export * from "./errors/database-connection-error";
-// export * from "./errors/not-authorized-error";
-// export * from "./errors/not-found-error";
-// export * from "./errors/request-validation-error";
+export * from './errors/bad-request-error';
+export * from './errors/custom-error';
+export * from './errors/forbidden-error';
+export * from './errors/database-connection-error';
+export * from './errors/not-authorized-error';
+export * from './errors/not-found-error';
+export * from './errors/request-validation-error';
 
 // export * from "./middlewares/current-user";
 // export * from "./middlewares/error-handler";
